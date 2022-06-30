8e855a4d3002a20f030c77375514e19661c4a56d
Hans Tognon <learningway403@gmail.com>
2022-07-01 00:02:02 +0100

    t5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Using printf to print a string to stdout
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
