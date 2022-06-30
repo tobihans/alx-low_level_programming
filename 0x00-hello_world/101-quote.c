a8472deb236cd3ba123ec322a863d9e63d450905
Hans Tognon <learningway403@gmail.com>
2022-07-01 00:38:34 +0100

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Using printf to print a string to stdout
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-	fprintf(stderr, "%s\n", "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
 	return (0);
 }
