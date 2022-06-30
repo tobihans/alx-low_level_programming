01c8c7750968ca573dbc835fd8a37ff4c94d5464
Hans Tognon <learningway403@gmail.com>
2022-07-01 00:32:47 +0100

    101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Using printf to print a string to stdout
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	fprintf(stderr, "%s\n", "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (0);
+}
