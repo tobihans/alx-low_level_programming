1c3e5ffdbfadea74d3ee3c4eff31c8bba24778fe
Hans Tognon <learningway@gmail.com>
2022-07-04 22:53:58 +0100

    t2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - alphabet
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	int i;
+	for (i=97; i <= 122; i++)
+		putchar(i);
+	putchar(10);
+	return (0);
+}
