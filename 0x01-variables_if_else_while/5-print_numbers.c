e6b70bb853b215a8fef3c1a004126adb2a8da64e
Hans Tognon <learningway@gmail.com>
2022-07-04 23:12:07 +0100

    t5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - alphabet
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+		putchar(i);
+	putchar(10);
+	return (0);
+}
