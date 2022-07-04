8c21f8553f604b91436d1abbc09ebb364e0daae5
Hans Tognon <learningway@gmail.com>
2022-07-04 23:46:59 +0100

    t100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - digits
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 48; i <= 57; i++)
+	{
+		for (j = 49; j <= 57; j++)
+		{
+			if (j <= i)
+				continue;
+
+			putchar(i);
+			putchar(j);
+
+			if (i < 56)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+
+		
+	}
+	putchar(10);
+	return (0);
+}
