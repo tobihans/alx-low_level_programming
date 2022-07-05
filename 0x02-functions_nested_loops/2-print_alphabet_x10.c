105380a6b9abbe1529afb34c0eedb000d68d64b4
Hans Tognon <learningway@gmail.com>
2022-07-06 00:19:03 +0100

    t2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints alphabet 10 times
+ *
+ * Returns void - always
+ */
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 97; j < 123; j++)
+			_putchar(j);
+		_putchar(10);
+	}
+}
