d25d2267cbba1d85c62f7ba31a2450adc1182564
Hans Tognon <learningway403@gmail.com>
2022-07-06 22:03:41 +0100

    t8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ */
+void jack_bauer(void)
+{
+	int i, j;
+
+	for (i = 0; i < 24; i++)
+	{
+		for (j = 0; j < 60; j++)
+		{
+			_putchar('0' + (i / 10));
+			_putchar('0' + (i % 10));
+			_putchar(58);
+			_putchar('0' + (j / 10));
+			_putchar('0' + (j % 10));
+		}
+	}
+}
