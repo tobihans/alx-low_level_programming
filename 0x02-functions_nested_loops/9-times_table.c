5fad8f779cd4fdcbebefedb448005f29519fa571
Hans Tognon <learningway403@gmail.com>
2022-07-06 22:16:32 +0100

    t9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,11 +7,31 @@
  */
 void times_table(void)
 {
-	int i, j;
+	int i, j, result;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
-			_putchar(i);
+		{
+			result = i * j;
+
+			if (j > 0)
+			{
+				_putchar(23);
+				_putchar(23);
+			}
+			
+			if (result > 10)
+			{
+				_putchar('0' + (result / 10));
+				_putchar('0' + (result % 10));
+			}
+			else
+			{
+				_putchar('0' + result);
+			}
+			_putchar(44);
+		}
+		_putchar(10);
 	}
 }
