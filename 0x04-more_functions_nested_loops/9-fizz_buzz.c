9968eebca73fa2c7d2f5dda986e9ccd024914206
Hans Tognon <learningway403@gmail.com>
2022-07-07 23:13:19 +0100

    0x04t9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - FizzBUzz entrypoint
+ *
+ * Return: void
+ */
+int main(void)
+{
+	int i;
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 5 == 0 && i % 3 == 0)
+		{
+			printf("%s", "FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("%s", "Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("%s", "Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+
+		if (i < 100)
+			_putchar(32);
+	}
+	return (0);
+}
