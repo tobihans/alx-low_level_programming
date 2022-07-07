29880553569fc0b0aaf9af2e2c2f36cf5d070b61
Hans Tognon <learningway403@gmail.com>
2022-07-07 22:59:35 +0100

    0x04 t8

---
+++ 0x04-more_functions_nested_loops/8-print-square.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_square - Prints a square
+ * @n: THe square side
+ *
+ * Return: void
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++)
+			_putchar(35);
+		_putchar(10);
+	}
+}
