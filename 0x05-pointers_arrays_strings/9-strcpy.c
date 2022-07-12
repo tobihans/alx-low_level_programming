422935b57c94169ffcfe08c00173fae8cde611b2
Hans Tognon <learningway403@gmail.com>
2022-07-12 13:39:51 +0100

    0x05 t9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * strcpy - Copy a string from a pointer to another one
+ * @src: THe source string
+ * @dest: THe destination buffer
+ *
+ * Return: char *, the pointer to the destination
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *iter, *iter2;
+
+	iter2 = dest;
+
+	for (iter = src; *iter; iter++)
+	{
+		*iter2 = *iter;
+		iter2++;
+	}
+
+	*iter2 = '\0';
+
+	return dest;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -19,4 +19,6 @@ void puts2(char *str);
 
 void print_array(int *a, int n);
 
+char *_strcpy(char *dest, char *src);
+
 #endif
