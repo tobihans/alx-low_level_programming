9003bdf6b1464d0d09c32770e8dcd807133d73b6
Hans Tognon <learningway403@gmail.com>
2022-07-12 12:59:25 +0100

    0x05 t6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints evry even char of a string
+ * @str: THe string
+ *
+ * Return: void
+ */
+void puts2(char *str)
+{
+	char *iter;
+
+	for (iter = str; *iter; iter += 2)
+		_putchar(*iter);
+}
