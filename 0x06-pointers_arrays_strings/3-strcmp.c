7506333530508aedc6aa4844bc5b531a8aab7fd0
Hans Tognon <learningway403@gmail.com>
2022-07-15 14:46:47 +0100

    0x06 t3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* _strcmp - Compares two strings in the same way as strcmp
+* @s1: THe first string
+* @s2: The second string
+*
+* Return: 0 if they're equal, -1 if s1 is less than s2 or 1
+*/
+int _strcmp(char *s1, char *s2)
+{
+	char *iter = s1, *iter2 = s2;
+
+	while (*iter && *iter2)
+	{
+		if (*iter < *iter2)
+			return (-1);
+
+		if (*iter > *iter2)
+			return (1);
+		iter++;
+		iter2++;
+	}
+
+	return (0);
+}
