45b2fd1be2601e316ad5d0ae6a5d593b1af09253
Hans Tognon <learningway403@gmail.com>
2022-07-19 00:04:23 +0100

    0x07 t4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strpbrk - Search a string for any other set of bytes
+ * @s: THe string
+ * @accept: THe set of char to search within string
+ *
+ * Return: the pointer to the char matching or NULL
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	char *iter, *result;
+
+	for (iter = s; *iter; iter++)
+	{
+		result = _strchr(accept, *iter);
+	
+		if (*result)
+			break;
+	}
+	return (result);
+}
+
+/**
+ * _strchr - Locates a char in a string
+ * @s: THe string to search on
+ * @c: THe character to search for
+ *
+ * Return: char*, a pointer to the first occurence of the char or NULL
+ */
+char *_strchr(char *s, char c)
+{
+	char *iter, *result = NULL;
+
+	for (iter = s; *iter; iter++)
+		if (*iter == c)
+			result = iter;
+
+	return (result);
+}
