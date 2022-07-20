0194ce494f6cab571774426cea6de4e065fee07e
Hans Tognon <learningway403@gmail.com>
2022-07-20 21:00:34 +0100

    0x08 t4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - Raise x to the power of y and return the result
+ * @x: The value to Raise
+ * @y: The power
+ *
+ * Return: -1 if y < 0, or the result
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return 1;
+
+	return (x * _pow_recursion(x, y - 1));
+}
