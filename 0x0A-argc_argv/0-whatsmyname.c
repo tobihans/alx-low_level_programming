081cdf725c0e2e5cc4bd647bcd86f71ffb9397a7
Hans Tognon <learningway403@gmail.com>
2022-07-22 20:48:13 +0100

    0x0A t0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Prints it's executable name and quits
+ * 
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
