6fc3074006a65a84b532ebf686c5f90882092f95
Hans Tognon <learningway403@gmail.com>
2022-07-22 20:52:45 +0100

    0x0A t1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Prints it's executable name and quits
+ *
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
