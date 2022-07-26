bcd57a22ac1c076380dd69c8eddb38e8aa729847
Hans Tognon <learningway@gmail.com>
2022-07-26 14:33:23 +0100

    0x0B t3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -18,7 +18,12 @@ int **alloc_grid(int width, int height)
 	p = (int **) malloc(sizeof(int *) * height);
 
 	for (i = 0; i < height; i++)
+	{
 		p[i] = (int *) malloc(sizeof(int) * width);
+		
+		if (p[i] == NULL)
+			return (NULL);
+	}
 
 	for (i = 0; i < height; i++)
 		for (j = 0; j < width; j++)
