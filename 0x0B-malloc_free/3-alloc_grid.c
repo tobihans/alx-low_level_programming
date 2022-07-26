8ae5dbe4541da543deb975369164c7c9c8b5db8c
Hans Tognon <learningway@gmail.com>
2022-07-26 14:32:33 +0100

    0x0B t3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * alloc_grid - Allocates a two dimensionnal array
+ * @width: Width
+ * @height: Height
+ *
+ * Return: a pointer to the array
+ */
+int **alloc_grid(int width, int height)
+{
+	int **p, i, j;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	p = (int **) malloc(sizeof(int *) * height);
+
+	for (i = 0; i < height; i++)
+		p[i] = (int *) malloc(sizeof(int) * width);
+
+	for (i = 0; i < height; i++)
+		for (j = 0; j < width; j++)
+			p[i][j] = 0;
+
+	return (p);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -9,5 +9,7 @@ char *_strdup(char *str);
 
 char *str_concat(char *s1, char *s2);
 
+int **alloc_grid(int width, int height);
+
 #endif
 
