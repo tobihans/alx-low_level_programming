c35cd35bbc5274038e33562fec37b06db94f1dec
Hans Tognon <learningway@gmail.com>
2022-07-26 14:39:08 +0100

    0x0B t3 - valgrind issues

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -18,7 +18,10 @@ int **alloc_grid(int width, int height)
 	p = (int **) malloc(sizeof(int *) * height);
 
 	if (p == NULL)
+	{
+		free(p);
 		return (NULL);
+	}
 
 	for (i = 0; i < height; i++)
 	{
